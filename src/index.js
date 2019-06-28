f6fac4709ff693fe59d45ad9988cd5c05f64fd26
Daniel Platon <platon@adobe.com>
2019-06-28 12:09:33 +0300

    Remove sample data

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import {ApolloServer, gql} from 'apollo-server';
 import typeDefs from './schema.graphql';
 import Query from './resolvers/Query';
-
 require('dotenv');
-const books = [
-    {
-        title: 'The Stand',
-        author: 'Stephen King'
-    },
-    {
-        title: 'Foundation',
-        author: 'Isaac Asimov'
-    }
-];
 
-console.log(Query);
 const server = new ApolloServer({typeDefs, resolvers: {Query}});
 
 server.listen().then(({url}) => {
