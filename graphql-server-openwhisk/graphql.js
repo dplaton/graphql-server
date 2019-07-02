2ff59221c14f20e9d1a77217b256391f95432431
Daniel Platon <platon@adobe.com>
2019-07-02 12:29:53 +0300

    [WIP] Implement a version of Apollo Server that works in a serverless environment

---
+++ graphql-server-openwhisk/graphql.js
@@ -0,0 +1,22 @@
+// graphql.js
+
+const {gql} = require('apollo-server-core');
+const ApolloServer = require('./ApolloServer');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!'
+    }
+};
+console.log(`Building the server...`);
+const server = new ApolloServer({typeDefs, resolvers});
+
+module.exports.graphqlHandler = server.createHandler();
---
+++ graphql-server-openwhisk/openwhiskApollo.js
@@ -0,0 +1,48 @@
+'use strict';
+const runHttpQuery = require('apollo-server-core').runHttpQuery;
+const {Headers} = require('apollo-server-env');
+
+function openwhiskApollo(options) {
+    if (!options) {
+        throw new Error('Need some options');
+    }
+    const graphqlHandler = (req, res) => {
+        const hasPostBody = req.body && Object.keys(req.body).length > 0;
+        if (req.method === 'POST' && !hasPostBody) {
+            res.status(500).send('POST body missing.');
+            return;
+        }
+
+        runHttpQuery([req, res], {
+            method: req.method,
+            options: options,
+            query: hasPostBody ? req.body : req.query,
+            request: {
+                url: req.url,
+                method: req.method,
+                headers: new Headers(req.headers) // ? Check if this actually works
+            }
+        }).then(
+            ({graphqlResponse, responseInit}) => {
+                res.status(200)
+                    .set(responseInit.headers)
+                    .send(graphqlResponse);
+            },
+            error => {
+                if ('HttpQueryError' !== error.name) {
+                    res.status(500).send(error);
+                    return;
+                }
+                res.status(error.statusCode)
+                    .set(error.headers)
+                    .send(error.message);
+            }
+        );
+    };
+
+    // const name = params.name || 'World';
+    // return {payload: `Hello, ${name}!`};
+    return graphqlHandler;
+}
+
+module.exports.openwhiskApollo = openwhiskApollo;
